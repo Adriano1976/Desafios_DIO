15602faf3dc617de12400bf714f18a3a81dc8103
Adriano Santos <adriradiologista@hotmail.com.br>
2024-05-09 13:08:11 -0300

    Add files via upload

--- Python/O Robô inteligente.py
+++ Python/O Robô inteligente.py
@@ -1,11 +1,11 @@
-/* O texto descreve um desafio onde você foi contratado pela empresa DIO Robots para 
+""" O texto descreve um desafio onde você foi contratado pela empresa DIO Robots para 
 programar um robô capaz de classificar números em negativo, positivo ou zero. Para isso, 
 você deve criar uma função de classificação que receba um número como parâmetro e 
 retorne a mensagem "negativo!" ou "positivo!" de acordo com sua categoria. O programa 
 deve ser executado continuamente, permitindo que o usuário insira vários números, 
 encerrado quando o usuário digitar zero. Após cada entrada, o robô deve classificar 
 e exibir a mensagem correspondente. Ao final, o programa deve exibir a quantidade 
-de números positivos, negativos e zeros inseridos. */
+de números positivos, negativos e zeros inseridos. """
 
 # Função para classificar um número como positivo, negativo ou zero
 def classificar_numero(numero):
